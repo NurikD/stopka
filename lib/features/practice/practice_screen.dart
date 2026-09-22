d0b46bc0a78916818757a2e7d6fca6aca697dd5f
NurikD <duishoev0123@gmail.com>
2026-09-23 03:59:05 +0600

    add practice screen stub

---
+++ lib/features/practice/practice_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PracticeScreen extends StatelessWidget {
+  const PracticeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Практика')),
+      body: const Center(
+        child: Text('Диктант и практика в предложениях появятся позже.'),
+      ),
+    );
+  }
+}
