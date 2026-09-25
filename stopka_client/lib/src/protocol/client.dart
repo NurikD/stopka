21cf3d5773921d1e36f66d191c61bb671153a418
NurikD <duishoev0123@gmail.com>
2026-09-25 06:00:55 +0600

    add the serverpod server and client to the monorepo, CI for both

---
+++ stopka_client/lib/src/protocol/client.dart
@@ -0,0 +1,75 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+// ignore_for_file: invalid_use_of_internal_member
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:async' as _ida;
+import 'package:http/http.dart' as _i85jenna;
+import 'package:serverpod_client/serverpod_client.dart' as _isc;
+import 'package:stopka_client/src/protocol/greetings/greeting.dart'
+    as _iew7sofi;
+import 'protocol.dart' as _il2as5qe;
+
+/// This is an example endpoint that returns a greeting message through
+/// its [hello] method.
+/// {@category Endpoint}
+class EndpointGreeting extends _isc.EndpointRef {
+  EndpointGreeting(_isc.EndpointCaller caller) : super(caller);
+
+  @override
+  String get name => 'greeting';
+
+  /// Returns a personalized greeting message: "Hello {name}".
+  _ida.Future<_iew7sofi.Greeting> hello(String name) =>
+      caller.callServerEndpoint<_iew7sofi.Greeting>(
+        'greeting',
+        'hello',
+        {'name': name},
+      );
+}
+
+class Client extends _isc.ServerpodClientShared {
+  Client(
+    String host, {
+    dynamic securityContext,
+    Duration? streamingConnectionTimeout,
+    Duration? connectionTimeout,
+    Function(
+      _isc.MethodCallContext,
+      Object,
+      StackTrace,
+    )?
+    onFailedCall,
+    Function(_isc.MethodCallContext)? onSucceededCall,
+    bool? disconnectStreamsOnLostInternetConnection,
+    _i85jenna.Client? httpClientOverride,
+  }) : super(
+         host,
+         _il2as5qe.Protocol(),
+         securityContext: securityContext,
+         streamingConnectionTimeout: streamingConnectionTimeout,
+         connectionTimeout: connectionTimeout,
+         onFailedCall: onFailedCall,
+         onSucceededCall: onSucceededCall,
+         disconnectStreamsOnLostInternetConnection:
+             disconnectStreamsOnLostInternetConnection,
+         httpClientOverride: httpClientOverride,
+       ) {
+    greeting = EndpointGreeting(this);
+  }
+
+  late final EndpointGreeting greeting;
+
+  @override
+  Map<String, _isc.EndpointRef> get endpointRefLookup => {'greeting': greeting};
+
+  @override
+  Map<String, _isc.ModuleEndpointCaller> get moduleLookup => {};
+}
